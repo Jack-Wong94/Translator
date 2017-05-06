5e3aba6fbfd74901d10acf0415360ee5135d5ec2
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2017-05-06 12:52:56 +1200

    Add project files.

---
+++ Translator/Translator/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json;
+namespace Translator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void TakePhoto(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            string text = "text="+"apple";
+            string lang = "lang="+"en-zh";
+            string translatorKey = "key="+"trnsl.1.1.20170505T130736Z.9886d1e879de6303.6a534fd32397ecba37a3c120449d094ba135e6a1";
+            string uri = "https://translate.yandex.net/api/v1.5/tr.json/translate?" + translatorKey +"&"+ text+"&" + lang;
+            Button button = sender as Button;
+            button.Text = "You click it";
+            //HttpResponseMessage response = await client.GetAsync(uri);
+            string result = await client.GetStringAsync(uri);
+
+            var translatedTextModel = JsonConvert.DeserializeObject<TranslateTextModel>(result);
+            string translatedText = translatedTextModel.TranslatedText[0];
+            
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front,
+                Directory = "Moodify",
+                Name = $"{DateTime.UtcNow}.jpg",
+                CompressionQuality = 92
+            });
+
+            if (file == null)
+            {
+                return;
+            }
+            else
+            {
+                //await DisplayAlert("Name", file.Path, "djfadsklj");
+                //var result = await UploadAndAnalyzeImage("https://en.wikipedia.org/wiki/Shogi#/media/File:Shogi_board_pieces_and_komadai.jpg");
+            }
+            try
+            {
+                VisionServiceClient VisionServiceClient = new VisionServiceClient("3bd57c38ada74daeb2d11c859f1c36bb");
+                //VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags };
+                string textMsg="";
+                var stream = file.GetStream();
+                //AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(stream, visualFeatures);
+                //var captions = analysisResult.Description.Captions;
+                OcrResults textResult = await VisionServiceClient.RecognizeTextAsync(stream, "en", true);
+                
+                foreach (Region region in textResult.Regions)
+                {
+                    foreach (Line line in region.Lines)
+                    {
+                        foreach (Word word in line.Words)
+                        {
+                            textMsg += word.Text + " ";
+                        }
+                    }
+                }
+                if (textMsg == "")
+                {
+                    HandwritingRecognitionOperation handWriteOp = await VisionServiceClient.CreateHandwritingRecognitionOperationAsync(stream);
+                    HandwritingRecognitionOperationResult handWriteResult = await VisionServiceClient.GetHandwritingRecognitionOperationResultAsync(handWriteOp);
+                    //handWriteResult.RecognitionResult.Lines[0].Words[0].Text;
+                    
+                    foreach(HandwritingTextLine line in handWriteResult.RecognitionResult.Lines)
+                    {
+                        foreach(HandwritingTextWord word in line.Words)
+                        {
+                            textMsg += word.Text + " ";
+                        }
+                    }
+
+                }
+
+                await DisplayAlert("Your text:", textMsg,"Cancel");
+                /*while (analysisResult.Description.Captions[i] != null)
+                {
+                   string textmsg = analysisResult.Description.Captions[i].Text;
+                    i++;
+                }*/
+                
+               
+                
+            }catch (Exception ex)
+            {
+
+            }
+
+        }
+        
+        /*private async Task<AnalysisResult> UploadAndAnalyzeImage(string imageFilePath)
+        {
+            // -----------------------------------------------------------------------
+            // KEY SAMPLE CODE STARTS HERE
+            // -----------------------------------------------------------------------  
+            //
+            // Create Project Oxford Computer Vision API Service client
+            //
+            VisionServiceClient VisionServiceClient = new VisionServiceClient("3bd57c38ada74daeb2d11c859f1c36bb");
+            VisualFeature[] visualFeatures = new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags };
+            AnalysisResult analysisResult = await VisionServiceClient.AnalyzeImageAsync(imageFilePath, visualFeatures);
+            return analysisResult;
+
+
+
+        }*/
+    }
+}
---
+++ Translator/Translator/TranslateTextModel/TranslateTextModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Translator
+{
+    public class TranslateTextModel
+    {
+        [JsonProperty(PropertyName = "text")]
+        public string[] TranslatedText { get; set; }
+
+        [JsonProperty(PropertyName = "lang")]
+        public string Lang { get; set; }
+    }
+}
